ac5305a552d0204b6475c16f50495907ba456a60
maany <imptodefeat@gmail.com>
2018-09-13 14:29:53 +0200

    update submitter

--- condor_submit_node/run_condor_submitter.sh
+++ condor_submit_node/run_condor_submitter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t condor_submitter .
-docker run --net condor_pool_net -itd --name condor_submitter -v /sys/fs/cgroup:/sys/fs/cgroup --privileged condor_submitter
+docker run --net condor_pool_net  -itd --name condor_submitter -v /sys/fs/cgroup:/sys/fs/cgroup --privileged condor_submitter
 docker exec -it condor_submitter systemctl start condor
 docker exec -it condor_submitter bash

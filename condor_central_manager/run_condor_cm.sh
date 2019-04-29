db4ae7108e82295fe0c293c7d74cd9e39f6468d4
maany <imptodefeat@gmail.com>
2019-04-29 11:55:33 +0200

    update condor_host

--- condor_central_manager/run_condor_cm.sh
+++ condor_central_manager/run_condor_cm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t condor_cm .
-docker run --net condor_pool_net --ip 10.0.2.5 -itd --name condor_cm -v /sys/fs/cgroup:/sys/fs/cgroup --privileged condor_cm
+docker run --net condor_pool_net --ip 10.0.2.50 -itd --name condor_cm -v /sys/fs/cgroup:/sys/fs/cgroup --privileged condor_cm
 docker exec -it condor_cm systemctl start condor
 docker exec -it condor_cm bash

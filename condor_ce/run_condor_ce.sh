b8c7c00f922d427874d3ce1299bc0eaa8cd8864a
maany <imptodefeat@gmail.com>
2019-04-30 09:49:52 +0200

    container name fix

--- condor_ce/run_condor_ce.sh
+++ condor_ce/run_condor_ce.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t condor_ce .
 docker run --net condor_pool_net --ip 10.0.2.51 -itd --name condor_ce -v /sys/fs/cgroup:/sys/fs/cgroup --privileged condor_ce
-docker exec -it condor_cm bash
+docker exec -it condor_ce bash

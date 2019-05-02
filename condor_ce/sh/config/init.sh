56f86943f793f337616eaa7750dc6de814236fc2
maany <imptodefeat@gmail.com>
2019-05-02 13:09:39 +0200

    update init.sh

--- condor_ce/sh/config/init.sh
+++ condor_ce/sh/config/init.sh
@@ -7,6 +7,6 @@ cp /etc/simple_grid/config/condor_mapfile /etc/condor-ce/
 cp /etc/simple_grid/config/hostcert.pem /etc/grid-security/
 cp /etc/simple_grid/config/hostkey.pem /etc/grid-security/
 chmod 600 /etc/grid-security/hostkey.pem
-cp -rf /etc/simple_grid/certificates /etc/grid-security
-cp -rf /etc/simple_grid/gsi.conf /etc/grid-security
-cp -rf /etc/simple_grid/sleep_job /etc/simple_grid/sleep_job
+cp -rf /etc/simple_grid/config/certificates /etc/grid-security
+cp -rf /etc/simple_grid/config/gsi.conf /etc/grid-security
+cp -rf /etc/simple_grid/config/sleep_job /etc/simple_grid/sleep_job

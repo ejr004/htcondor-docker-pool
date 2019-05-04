ff82b04b725fb4f58d095b240d0a6b6acadff7d8
maany <imptodefeat@gmail.com>
2019-05-04 21:51:28 +0200

    container name = condor-ce01

--- condor_ce/run_condor_ce.sh
+++ condor_ce/run_condor_ce.sh
@@ -3,10 +3,10 @@ docker build -t condor_ce .
 docker run --net condor_pool_net \
     --ip 10.0.2.151 \
     --hostname=$(hostname) \
-    -itd --name condor_ce \
+    -itd --name condor_ce01 \
     -v /sys/fs/cgroup:/sys/fs/cgroup \
     -v $(pwd)/sh:/etc/simple_grid \
     --privileged \
     condor_ce
 #docker exec -it condor_ce /etc/simple_grid/condig/init.sh
-docker exec -it condor_ce bash
+docker exec -it condor_ce01 bash

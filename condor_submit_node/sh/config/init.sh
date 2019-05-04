16afa40e0e65844e4ba7ec82a598087ec45ae1ba
maany <imptodefeat@gmail.com>
2019-05-04 19:18:18 +0200

    update init script

--- condor_submit_node/sh/config/init.sh
+++ condor_submit_node/sh/config/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+systemctl start crond
 systemctl start fetch-crl-cron
-mv /etc/simple_grid/config/user* /home/condor_user/.globus
+cp /etc/simple_grid/config/user* /home/condor_user/.globus
 chown -R condor_user /home/condor_user/.globus

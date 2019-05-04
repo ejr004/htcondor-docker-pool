720a6dfd0ef31a423a4d45d0380dc07e7fe15d8f
maany <imptodefeat@gmail.com>
2019-05-04 21:28:01 +0200

    changing condor_ce to 10.0.2.151

--- condor_submit_node/run_condor_submitter.sh
+++ condor_submit_node/run_condor_submitter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t condor_submitter .
-docker run --net condor_pool_net -itd --name condor_submitter -v /sys/fs/cgroup:/sys/fs/cgroup -v $(pwd)/sh/config:/etc/simple_grid/config --privileged --add-host simple-condor-ce.cern.ch:10.0.2.51 condor_submitter
+docker run --net condor_pool_net -itd --name condor_submitter -v /sys/fs/cgroup:/sys/fs/cgroup -v $(pwd)/sh/config:/etc/simple_grid/config --privileged --add-host simple-condor-ce.cern.ch:10.0.2.151 condor_submitter
 docker exec -it condor_submitter systemctl start condor
 docker exec -it condor_submitter bash

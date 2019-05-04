4015eb9402ebb1f51130af2c83e450f75074570f
maany <imptodefeat@gmail.com>
2019-05-04 22:53:43 +0200

    fix ce port

--- condor_ce/run_condor_ce.sh
+++ condor_ce/run_condor_ce.sh
@@ -2,7 +2,7 @@
 docker build -t condor_ce .
 docker run --net condor_pool_net \
     --ip 10.0.2.151 \
-    -p 9618:9618 \
+    -p 9619:9619 \
     --hostname=$(hostname) \
     -itd --name condor_ce01 \
     -v /sys/fs/cgroup:/sys/fs/cgroup \

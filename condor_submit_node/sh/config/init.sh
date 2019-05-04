330beae89decd2e5579171a0b13a78b4a6a48858
maany <imptodefeat@gmail.com>
2019-05-04 18:58:57 +0200

    cleanup init script

--- condor_submit_node/sh/config/init.sh
+++ condor_submit_node/sh/config/init.sh
@@ -2,5 +2,3 @@
 systemctl start fetch-crl-cron
 mv /etc/simple_grid/config/user* /home/condor_user/.globus
 chown -R condor_user /home/condor_user/.globus
-su condor_user && voms-proxy-init && voms-proxy-info
-cd /home/condor_user/sleep_job

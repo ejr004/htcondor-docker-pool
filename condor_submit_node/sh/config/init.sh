6e843e01290b7b820ed93e5e4929151f74f6eabd
maany <imptodefeat@gmail.com>
2019-05-04 18:54:56 +0200

    update init script

--- condor_submit_node/sh/config/init.sh
+++ condor_submit_node/sh/config/init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-mv /etc/simple_grid/config/user* /home/condor_user/.globus
-su condor_user && voms-proxy-init && voms-proxy-info
 systemctl start fetch-crl-cron
+mv /etc/simple_grid/config/user* /home/condor_user/.globus
+chown -R condor_user /home/condor_user/.globus
+su condor_user && voms-proxy-init && voms-proxy-info
+cd /home/condor_user/sleep_job

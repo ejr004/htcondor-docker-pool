297bbd30b942147d63a8e0f11e63658c266e29a4
Mayank Sharma <imptodefeat@gmail.com>
2019-05-05 19:44:49 +0200

    switch to host networks

--- condor_ce/run_condor_ce.sh
+++ condor_ce/run_condor_ce.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 docker build -t condor_ce .
-docker run --net condor_pool_net \
-    --ip 10.0.2.151 \
-    -p 9619:9619 \
-    --hostname=$(hostname) \
-    -itd --name condor_ce01 \
+docker run --net host \
+    -itd \
+    --name condor_ce02 \
     -v /sys/fs/cgroup:/sys/fs/cgroup \
     -v $(pwd)/sh:/etc/simple_grid \
     --privileged \
     condor_ce
 #docker exec -it condor_ce /etc/simple_grid/condig/init.sh
-docker exec -it condor_ce01 bash
+docker exec -it condor_ce02 bash
--- condor_execute_node/run_condor_executor.sh
+++ condor_execute_node/run_condor_executor.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 docker build -t condor_executor .
-docker run --net condor_pool_net  -itd --name condor_executor -v /sys/fs/cgroup:/sys/fs/cgroup --mount type=bind,source=/cvmfs,target=/cvmfs,bind-propagation=shared --privileged condor_executor
+docker run --net host  \
+    -itd \
+    --name condor_executor \
+    -v /sys/fs/cgroup:/sys/fs/cgroup \
+    --mount type=bind,source=/cvmfs,target=/cvmfs,bind-propagation=shared \
+    --privileged \
+    condor_executor
 docker exec -it condor_executor systemctl start condor
 docker exec -it condor_executor bash
